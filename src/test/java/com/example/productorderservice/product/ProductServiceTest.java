4c0382c9d3304f304ca937694daf26444283a985
[yousincha] <[yousincha@naver.com]>
2024-04-30 14:41:47 +0900

    20240430-3 (상품 수정기능 구현, 스프링부트 테스트 전환) commit

---
+++ src/test/java/com/example/productorderservice/product/ProductServiceTest.java
@@ -0,0 +1,46 @@
+package com.example.productorderservice.product;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@SpringBootTest
+class ProductServiceTest {
+
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    void 상품수정(){
+        productService.addProduct(ProductSteps.상품등록요청_생성());
+        final Long productId = 1L;
+        final UpdateProductRequest request = new UpdateProductRequest("상품수정", 2000, DiscountPolicy.NONE);
+
+        productService.updateProduct(productId, request);
+
+        final ResponseEntity<GetProductResponse> response = productService.getProduct(productId);
+        final GetProductResponse productResponse = response.getBody();
+        assertThat(productResponse.name()).isEqualTo("상품수정");
+        assertThat(productResponse.price()).isEqualTo(2000);
+    }
+
+
+//    private static class StubProductPort implements ProductPort {
+//        public Product getProduct_will_return;
+//
+//        @Override
+//        public void save(final Product product) {
+//
+//        }
+//
+//        @Override
+//        public Product getProduct(final Long productId) {
+//            return getProduct_will_return;
+//        }
+//    }
+}
