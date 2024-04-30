e491c1300568c78e2d3cb3f3bdc29de9894a5002
[yousincha] <[yousincha@naver.com]>
2024-04-30 11:09:07 +0900

    20240430-2 (API 테스트 전환) commit

--- src/test/java/com/example/productorderservice/product/ProductApITest.java
+++ src/test/java/com/example/productorderservice/product/ProductApITest.java
@@ -1,6 +1,9 @@
 package com.example.productorderservice.product;
 
 import com.example.productorderservice.ApiTest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
@@ -15,4 +18,17 @@ class ProductApITest extends ApiTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
+    @Test
+    void 상품조회() {
+        ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
+        Long productId = 1L;
+
+        final ExtractableResponse<Response> response= RestAssured.given().log().all()
+                .when()
+                .get("/products/{productId}",productId)
+                .then().log().all()
+                .extract();
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getString("name")).isEqualTo("상품명");
+    }
 }
